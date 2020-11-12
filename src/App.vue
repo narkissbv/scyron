e59a7453ffea9b077d3935281b63e263c94c8cd5
Matan Narkiss <dv.narxx@gmail.com>
2020-11-12 17:41:08 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Scyron/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Scyron from './components/Scyron';
+
+export default {
+  name: 'App',
+
+  components: {
+    Scyron,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Scyron.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-container>
+      <h1>Scyron Home Assignment</h1>
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
