6206cde3583225a02de13ae7973398636dd55e14
Matan Narkiss <dv.narxx@gmail.com>
2020-12-06 13:53:19 +0200

    add vue-vis-network example

--- src/components/Scyron.vue
+++ src/components/Scyron.vue
@@ -11,6 +11,10 @@
         <h3 @click="showTask2 = !showTask2">Task 2: Word Cloud</h3>
       </v-card>
       <WordCloud v-if="showTask2"/>
+      <v-card>
+        <h3 @click="showTask3 = !showTask3">Task3: vis-network</h3>
+      </v-card>
+      <VisNetwork v-if="showTask3"/>
     </v-container>
   </div>
 </template>
@@ -18,16 +22,19 @@
 <script>
 import DataTable from './DataTable'
 import WordCloud from './WordCloud'
+import VisNetwork from './VisNetwork'
 export default {
   data () {
     return {
       showTask1: false,
-      showTask2: false
+      showTask2: false,
+      showTask3: false
     }
   },
   components: {
     DataTable,
-    WordCloud
+    WordCloud,
+    VisNetwork
   }
 }
 </script>
